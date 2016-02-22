d50013ebd83da19a18a65645ea1eea77344f633c
niqdev <niqdev@gmail.com>
2016-02-22 08:09:30 +0100

    add javadocs

--- library/src/main/java/com/github/niqdev/mjpeg/Mjpeg.java
+++ library/src/main/java/com/github/niqdev/mjpeg/Mjpeg.java
@@ -28,7 +28,13 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 /**
+ * A library wrapper for handle mjpeg Ipcam streams.
  *
+ * @see
+ * <ul>
+ *     <li><a href="https://bitbucket.org/neuralassembly/simplemjpegview">simplemjpegview</a></li>
+ *     <li><a href="https://code.google.com/archive/p/android-camera-axis">android-camera-axis</a></li>
+ * </ul>
  */
 public class Mjpeg {
 
@@ -48,14 +54,32 @@ public class Mjpeg {
         this.type = type;
     }
 
+    /**
+     * Uses {@link Type.DEFAULT} implementation.
+     *
+     * @return Mjpeg instance
+     */
     public static Mjpeg newInstance() {
         return new Mjpeg(Type.DEFAULT);
     }
 
+    /**
+     * Choose among {@link com.github.niqdev.mjpeg.Mjpeg.Type} implementations.
+     *
+     * @return Mjpeg instance
+     */
     public static Mjpeg newInstance(Type type) {
         return new Mjpeg(type);
     }
 
+    /**
+     * Configure authentication.
+     *
+     * @param username
+     * @param password
+     *
+     * @return Mjpeg instance
+     */
     public Mjpeg credential(String username, String password) {
         // TODO
         return this;
@@ -67,6 +91,7 @@ public class Mjpeg {
                 HttpURLConnection urlConnection = (HttpURLConnection) new URL(url).openConnection();
                 InputStream inputStream = urlConnection.getInputStream();
                 switch (type) {
+                    // handle multiple implementations
                     case DEFAULT: return Observable.just(new MjpegInputStreamDefault(inputStream));
                     case NATIVE: return Observable.just(new MjpegInputStreamNative(inputStream));
                 }
@@ -79,11 +104,11 @@ public class Mjpeg {
         .observeOn(AndroidSchedulers.mainThread());
     }
 
-    // TODO builder.xxx.native().build() {boolean}
     // - MjpegInputStream
     // - MjpegView
 
-    public static MjpegInputStreamDefault readDefault(String url) {
+    // TODO deprecated
+    private MjpegInputStreamDefault readDefault(String url) {
         try {
             DefaultHttpClient httpclient = new DefaultHttpClient();
             HttpResponse res = httpclient.execute(new HttpGet(URI.create(url)));
