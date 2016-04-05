ed4486af4d94c7cb8525c911a943c1f1f74baa24
niqdev <niqdev@gmail.com>
2016-04-05 18:35:20 +0200

    update compileJni script

--- compileJni.sh
+++ compileJni.sh
@@ -2,6 +2,7 @@
 
 JNI_SRC="mjpeg-view/src/main"
 
+rm -fr $JNI_SRC/jniLibs
 ~/Library/Android/sdk/ndk-bundle/ndk-build -C $JNI_SRC
 cp -a $JNI_SRC/libs $JNI_SRC/jniLibs
 rm -fr $JNI_SRC/libs $JNI_SRC/obj
