49932324101e2c9ae0afaed0b3f71742f087fd3a
niqdev <niqdev@gmail.com>
2016-10-26 23:35:58 +0100

    add actionbar settings

--- app/src/main/java/com/github/niqdev/ipcam/MainActivity.java
+++ app/src/main/java/com/github/niqdev/ipcam/MainActivity.java
@@ -1,13 +1,17 @@
 package com.github.niqdev.ipcam;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.github.niqdev.ipcam.settings.SettingsActivity;
 import com.github.niqdev.mjpeg.DisplayMode;
 import com.github.niqdev.mjpeg.Mjpeg;
 import com.github.niqdev.mjpeg.MjpegView;
@@ -30,42 +34,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // TODO fix lower API
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ButterKnife.bind(this);
 
         // load default values first time
         PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
-
-        // TODO action bar startActivity(new Intent(this, SettingsActivity.class));
     }
+
+
 
     private String getPreference(String key) {
         return PreferenceManager
-                .getDefaultSharedPreferences(this)
-                .getString(key, "");
+            .getDefaultSharedPreferences(this)
+            .getString(key, "");
     }
 
     private DisplayMode calculateDisplayMode() {
         int orientation = getResources().getConfiguration().orientation;
         return orientation == Configuration.ORIENTATION_LANDSCAPE ?
-                DisplayMode.FULLSCREEN : DisplayMode.BEST_FIT;
+            DisplayMode.FULLSCREEN : DisplayMode.BEST_FIT;
     }
 
     private void loadIpCam() {
         Mjpeg.newInstance()
-                .credential(getPreference(PREF_AUTH_USERNAME), getPreference(PREF_AUTH_PASSWORD))
-                .open(getPreference(PREF_IPCAM_URL), TIMEOUT)
-                .subscribe(
-                        inputStream -> {
-                            mjpegView.setSource(inputStream);
-                            mjpegView.setDisplayMode(calculateDisplayMode());
-                            mjpegView.showFps(true);
-                        },
-                        throwable -> {
-                            Log.e(getClass().getSimpleName(), "mjpeg error", throwable);
-                            Toast.makeText(this, "Error", Toast.LENGTH_LONG).show();
-                        });
+            .credential(getPreference(PREF_AUTH_USERNAME), getPreference(PREF_AUTH_PASSWORD))
+            .open(getPreference(PREF_IPCAM_URL), TIMEOUT)
+            .subscribe(
+                inputStream -> {
+                    mjpegView.setSource(inputStream);
+                    mjpegView.setDisplayMode(calculateDisplayMode());
+                    mjpegView.showFps(true);
+                },
+                throwable -> {
+                    Log.e(getClass().getSimpleName(), "mjpeg error", throwable);
+                    Toast.makeText(this, "Error", Toast.LENGTH_LONG).show();
+                });
     }
 
     @Override
@@ -78,6 +83,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         mjpegView.stopPlayback();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
